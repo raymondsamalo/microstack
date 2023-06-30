18555f52dc93a3923f45146e158afd5bba65eaa7
raymond.samalo <raymond.samalo@ventitechnologies.com>
2023-06-30 17:17:03 +0800

    patches

--- huey/main.py
+++ huey/main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 from app.worker.config import huey
-from app.worker.tasks import add, script_run, script_retry, script_backoff
+from app.worker.tasks import script_run, script_retry, script_backoff
 if __name__ == '__main__':
-    result = add(1, 2)
-    print('1 + 2 = %s' % result.get(blocking=True))
     script_run("test.sh")
     script_retry("test.sh")
     script_backoff("test.sh")

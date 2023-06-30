502ff67b54610a7ba3c8d757632d36b5251fd5a2
raymond.samalo <raymond.samalo@ventitechnologies.com>
2023-06-30 17:15:50 +0800

    working example to run scripts

--- huey/app/worker/config.py
+++ huey/app/worker/config.py
@@ -1,4 +1,28 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
 from huey import RedisHuey
 import os 
-_password = os.environ['REDIS_PASSWORD']
-huey = RedisHuey('testing', password=_password)
+
+def create_huey():
+    password = os.getenv('REDIS_PASSWORD')
+    if password is not None:
+        huey = RedisHuey('tasks', password=password)
+    else:
+        huey = RedisHuey('tasks')
+    return huey
+
+def _configure_logger():
+    log_folder = os.getenv('HUEY_LOG_FOLDER')
+    if log_folder is not None:
+        log_file_name=os.path.join(log_folder, "huey_worker.log")
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(threadName)s %(message)s')
+        # use very short interval for this example, typical 'when' would be 'midnight' and no explicit interval
+        handler = TimedRotatingFileHandler(log_file_name, when="S", interval=30, backupCount=10)
+        handler.setFormatter(formatter)
+        logger = logging.getLogger('huey.consumer') #take logger and configure it
+        logger.addHandler(handler)
+        logger = logging.getLogger('huey') #take logger and configure it
+        logger.addHandler(handler)
+
+_configure_logger()
+huey=create_huey()
--- huey/app/worker/tasks.py
+++ huey/app/worker/tasks.py
@@ -1,5 +1,46 @@
+import logging
+import os
+from subprocess import Popen, PIPE, STDOUT
+from typing import List
 from .config import huey
+from .utils import HueyWorkerException, exp_backoff_task
+import uuid
+
+
+def _script(script_name: str, args: List[str], check_exitcode: bool = True):
+    logger = logging.getLogger('huey')  # take logger and configure it
+    script_path = os.path.join('/scripts', script_name)
+    commands = [script_path]
+    commands.extend(args)
+    command_string = ' '.join(commands)
+    command_uuid = uuid.uuid1()
+    logger.info('[%s] [run commands] %s', command_uuid, command_string)
+    process = Popen(commands, stdout=PIPE, stderr=STDOUT)
+    with process.stdout:
+        for line in process.stdout:
+            logger.info("[%s] [out] %s", command_uuid, line.decode())
+    exitcode = process.wait()  # 0 means success    logging.info(result.)
+    logger.info("[%s] [completed with] %d", command_uuid, exitcode)
+    if check_exitcode and exitcode != 0:
+        raise HueyWorkerException(
+            f"script {command_string} return {exitcode}", exitcode)
+
 
 @huey.task()
 def add(a, b):
     return a + b
+
+
+@exp_backoff_task(retries=5, retry_backoff=2)
+def script_backoff(script_name: str, args: List[str] = [], check_exitcode: bool = True):
+    _script(script_name, args, check_exitcode=check_exitcode)
+
+
+@huey.task(retries=2)
+def script_retry(script_name: str, args: List[str] = [], check_exitcode: bool = True):
+    _script(script_name, args, check_exitcode=check_exitcode)
+
+
+@huey.task()
+def script_run(script_name: str, args: List[str] = [], check_exitcode: bool = True):
+    _script(script_name, args, check_exitcode=check_exitcode)
